820a07b4fdc82c0513aaef5c391fa06b88b3b4d9
Julian Romera Juarez <julian.romera@gmail.com>
2013-11-18 11:39:38 +0100

    fix serious typos

--- install-nodejs.debian.sh
+++ install-nodejs.debian.sh
@@ -2,10 +2,14 @@
 
 apt-get install python
 
-$MAKE=`which make`
-$GIT=`which git`
+MAKE=`which make`
+GIT=`which git`
 
-$DOWNLOAD_DIR=/tmp/nodejs
+
+DOWNLOAD_DIR=/tmp/nodejs
+if [ ! -d $DOWNLOAD_DIR ];then
+  mkdir $DOWNLOAD_DIR
+fi
 
 alias cd_tmp="cd $DOWNLOAD_DIR"
 
@@ -15,24 +19,22 @@ if [ ! -x $MAKE ];then
   exit 1
 fi
 
-if [ ! -x $MAKE ];then
+if [ ! -x $GIT ];then
   echo ""
   echo "git installed? .. exiting"
   exit 1
 fi
 
 
-git clone https://github.com/joyent/node.git $DOWNLOAD_DIR
-cd_tmp;git checkout v0.6.8
+$GIT clone https://github.com/joyent/node.git $DOWNLOAD_DIR
+. cd_tmp && $GIT checkout v0.6.8
 
-if [ ! -d $DOWNLOAD_DIR ];then
-fi
 
 $DOWNLOAD_DIR/configure --openssl-libpath=/usr/lib/ssl
 
 
-cd_tmp;sudo $MAKE 
-cd_tmp;sudo $MAKE install
+. cd_tmp && sudo $MAKE 
+. cd_tmp && sudo $MAKE install
 RESULT=$?
 
 if [ $RESULT -ne 0 ];then
@@ -43,5 +45,5 @@ fi
 
 echo ""
 echo "Installing npm ..."
-cd_tmp;curl https://npmjs.org/install.sh | sudo sh
+. cd_tmp;curl https://npmjs.org/install.sh | sudo sh
 
